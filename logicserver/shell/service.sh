a84a22caea5528b14f347cd2b59c405e16bc4913
meilun <848843249@qq.com>
2024-05-15 03:32:08 -0700

    增加逻辑服

---
+++ logicserver/shell/service.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function start_func(){
+    nowPath=$(dirname "$0")
+    cd $nowPath
+    parentPath=$(dirname $(pwd))
+    ps -o command -C skynet | grep "./skynet/skynet ./config/login.conf" &> /dev/null
+    [ $? -eq 0 ] && echo "进程$1已经存在,禁止重复启动" && return
+    # 后台运行
+    nohup $parentPath/skynet/skynet $parentPath/config/login.conf &> /dev/null &
+    echo $parentPath, "服务器启动成功！"
+}
+
+function stop_func(){
+    echo ""
+}
+
+function kill_func(){
+    ps aux | grep "skynet"
+    echo ""
+}
+
+case $1 in
+    start)
+        start_func
+        echo "start shell";;
+    stop)
+        stop_func
+        echo "stop shell";;
+    kill)
+        kill_func
+        echo "kill shell";;
+esac
